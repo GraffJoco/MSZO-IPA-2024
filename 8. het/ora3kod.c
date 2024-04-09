a2a5f793915d1a32405d59e1cdf99449bcff25fb
Adan Yokota <adanyokota@gmail.com>
2024-04-09 23:55:15 +0200

    fv

--- 8. het/ora3kod.c
+++ 8. het/ora3kod.c
@@ -1,4 +1,4 @@
-
+/* IPA 8. HETI GYAKORLAT - STRUKTÚRÁK, FÜGGVÉNYEK */
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -13,24 +13,25 @@
 // Ha nem ad vissza értéket, akkor `void` a típusa
 
 // Példa: Függvény paraméterrel, ami értéket ad vissza
+double szumma(double a, double b, double c) {
+    return a + b + c;
+}
+//Atlag milyen?
+
+
+// Példa: Függvény paraméterekkel, ami nem ad vissza értéket (eljaras)
+void kiir(double val); //igy mindegy a sorrend, fuggveny prototipus mehetne .h fajba is, igy tobb fajlba is hasznalod
+
 double korTerulete(double R) {
-    return M_PI * R * R;
+    if (R < 0) return NAN; //Not a Number
+    else return M_PI * R * R;
 }
 // Ugyanaz, mint korTerulete: double -> double, korTerulete(R) = PI * R^2
 
-// Példa: Függvény paraméterekkel, ami nem ad vissza értéket
-void kiir(double val) {
-    printf("%lf\n", val);
-}
 
 
-/* STRUKTÚRÁK */
 
-// Struktúra - több, különbözű típusú, névvel ellátott érték egymás után egy,
-// kombinált változóban
-// Programozási tapasztalatokkal rendelkezőknek: primitív objektum, nincsenek tagfv-k
-
-// Példa: 3D vektor
+//3D vektor
 
 struct vektor {
     double x;
@@ -38,27 +39,73 @@ struct vektor {
     double z;
 }; // Pontosvessző a végére!
 
-// Más fajta típusok is lehetnek benne
-
-struct hallgato {
-    double KKI;
-    int eddigiKreditek;
-};
-
-// typedef: elé syntax, végére név
-
-
 // Struktúrák lehetnek függvényekben is!
-// Példa: vektorok skaláris szorzása
+struct vektor vektorSzoroz(struct vektor egyik, struct vektor masik) {
+    struct vektor v;
+    v.x=egyik.y*masik.z-egyik.z*masik.y;
+    v.y=egyik.z*masik.x-egyik.x*masik.z;
+    v.z=egyik.x*masik.y-egyik.y*masik.x;
+    return v;
+}
 
 double skalarSzoroz(struct vektor egyik, struct vektor masik) {
     return (egyik.x * masik.x) + (egyik.y * masik.y) + (egyik.z * masik.z);
 }
 
+// mutató használata
+void csere_p(int* a, int* b)        //a és b egészre mutató pointerek
+{
+	int tmp = *a; *a = *b; *b = tmp;
+}
+
+// Összetettebb példa: Fibonacci sor, rekurzió
+int Fibonacci(unsigned int ertek) {
+    if (ertek == 1 || ertek == 0) return ertek;
+    return Fibonacci(ertek - 1) + Fibonacci(ertek - 2);
+}
+
+
+double singyok(double x1, double x2, double eps) { //eps limit
+    // Bináris keresés használata gyökmeghatározásra (Intervallum felezés módszere)
+    //Azzal a feltetelel mukodik hogy egy gyok van csak az adott intervallumon
+
+        double xk = (x1 + x2) / 2;
+        int lepes = 0;
+        const int MAXLEPES = 1e3;
+
+        while (fabs(sin(xk)) > eps && lepes < MAXLEPES) // fabs: abszolút érték doublere, pl 0.1+0.2!=0.3
+        {
+            if (sin(x1) * sin(xk) > 0)
+                x1 = xk; // Átmegy-e a 0-n? -> határok elmozdítása
+            else
+                x2 = xk;
+
+            xk = (x1 + x2) / 2; // Középső érték újraszámolása
+            lepes++;
+            printf("Lepes: %i; xk: %.12lf\n", lepes, xk);
+        }
+        if (lepes == MAXLEPES) {
+            printf("Elerte a max lepesszamot, nincs eredmeny.\n"); // Végtelen ciklus megelőzése
+            return NAN;
+        }
+        return xk;
+}
+
 
 int main() {
     // Függvények tesztelése
-    kiir(korTerulete(2));
+    double szum =szumma(2,3,4);
+    printf("A szumma: %lf\n",szum);
+    kiir(szum); //pont annyi parameternek kell lennie
+
+    double beadott;
+    scanf_s("%lf", &beadott);
+    double megoldas = korTerulete(beadott);
+    if (isnan(megoldas)) {
+        printf("Nem ertelmes a sugar!\n");
+        return -1;
+    }
+    printf("A terület: %lf\n",megoldas);
 
     // Struktúrák
     // Példány létrehozása
@@ -69,16 +116,15 @@ int main() {
     // Struktúra függvények hívása
     printf("A ket vektor skalarszorzata: %lf\n", skalarSzoroz(vec1, vec2));
 
-    // Struktúra tömbök
-    struct vektor matrix[3];
-    
-    srand(time(NULL));
-    for (int i = 0; i < 3; i++) {
-        matrix[i].x = (double)rand() / (double)RAND_MAX;
-        matrix[i].y = (double)rand() / (double)RAND_MAX;
-        matrix[i].z = (double)rand() / (double)RAND_MAX;
-        printf("%lf, %lf, %lf\n", matrix[i].x, matrix[i].y, matrix[i].z);
-    }
+    int a=0,b=1;
+    csere(&a,&b);
+    kiir(Fibonacci(5));
 
-    
+    kiir(singyok(1,4,1e-6));
+
+}
+
+//kifejtes
+void kiir(double val) {
+    printf("%lf\n", val);
 }
